cfeb9c6816c0ce05db82b0a0769ba5b679d42965
Alex Siroli <alex.siroli@studio.unibo.it>
2023-03-13 11:47:43 +0100

    task 3c done

--- src/u01/Main.scala
+++ src/u01/Main.scala
@@ -23,13 +23,25 @@ object Main extends App {
 
   // Task 3b
   private val neg : (String => Boolean) => String => Boolean = f => s => !f(s)
-  //private def neg : (String => Boolean) => (String => Boolean) = f => (s => !f(s))
+  //private def neg : (String => Boolean) => String => Boolean = f => s => !f(s)
 
   private val empty: String => Boolean = _ == ""
   private val notEmpty = neg(empty)
+
   println(notEmpty("foo"))
   println(notEmpty(""))
   println(notEmpty("foo") && !notEmpty(""))
+  println()
+
+  // Task 3c
+  private def genericNeg[X] : (X => Boolean) => X => Boolean = f => s => !f(s)
+
+  private val positive: Int => Boolean = _ >= 0
+  private val negative = genericNeg(positive)
+
+  println(negative(-3))
+  println(negative(3))
+  println(negative(-4) && positive(0))
   println()
 
 }
