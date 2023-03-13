9b30f3e7bb4f097895a42027117aeeb8ff52b38c
Alex Siroli <alex.siroli@studio.unibo.it>
2023-03-13 11:17:13 +0100

    task 3a done

--- src/u01/Main.scala
+++ src/u01/Main.scala
@@ -4,4 +4,20 @@ object Main extends App {
 
   println("Hello, Scala")
 
+  private val positive1: Int => String = _ match
+    case x if x >= 0 => "positive"
+    case _ => "negative"
+
+  println(positive1(3))
+  println(positive1(0))
+  println(positive1(-3))
+
+  private def positive2: Int => String = _ match
+    case x if x >= 0 => "positive"
+    case _ => "negative"
+
+  println(positive2(3))
+  println(positive2(0))
+  println(positive2(-3))
+
 }
